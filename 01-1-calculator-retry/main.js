3bf653be88a47a82f1cfacad092952e8b17beea6
Jinsu <injnamek@gmail.com>
2024-09-20 20:23:06 +0900

    계산기 기본 기능 구현 완료

--- 01-1-calculator-retry/main.js
+++ 01-1-calculator-retry/main.js
@@ -30,48 +30,64 @@ const [zeroNode, oneNode, twoNode, threeNode, fourNode, fiveNode, sixNode, seven
   document.querySelector(".plus"),
 ];
 
-// 이벤트 리스너 등록
-
-// 숫자를 얻어 계산대에 올리는 함수
-const getNum = function (e) {
-  console.log(e.target.value);
+// 괄호와 숫자는 올라갈 때의 조건이 동일하므로 하나의 함수로 합침.
+const getValue = function (e) {
+  let value = e.target.value;
+  if (isResultDisplayed) {
+    resultNode.innerHTML = value;
+    isResultDisplayed = false;
+  } else {
+    resultNode.innerHTML += value;
+  }
   /*
   숫자가 계산대에 올라갈 때는
-  (1) 결과가 출력된 바로 다음이라면 결과값을 초기화 하고 새로운 수식 시작
-  (2) 아니라면 이어서 수식 계산
-  (3) 상태관리 함수를 false로 변경
+  (1) 결과가 출력된 바로 다음이라면 결과값을 초기화 하고 새로운 수식 시작 (o)
+  (2) 아니라면 이어서 수식 계산 (o)
+  (3) 상태관리 함수를 false로 변경 (o)
   */
 };
 
 // 연산자를 얻어 계산대에 올리는 함수
 const getOperator = function (e) {
-  console.log(e.target.value);
-
+  let operator = e.target.value;
+  let result = resultNode.innerHTML;
+  const operators = ["%", "/", "*", "-", "+"];
   /*
   연산자가 계산대에 올라가는 조건
-  (1) 숫자 없이 먼저 올라가지 못한다.
-  (2) 연산자 다음에 바로 올라가지 못한다.
+  (1) 숫자 없이 먼저 올라가지 못한다. (o)
+  (2) 연산자 다음에 바로 올라가지 못한다. (o)
+  */
+  if (/\d/.test(result) || !operators.includes(result.slice(-1))) {
+    resultNode.innerHTML += operator;
+    isResultDisplayed = false;
+  }
 
+  /*
   연산자가 계산대에 올라갈 때
-  (1) 결과가 출력된 상태든 아니든, 계산대에 올려져 있는 수식을 이어받아 계산
-  (2) 상태관리 함수를 false로 변경
+  (1) 결과가 출력된 상태든 아니든, 계산대에 올려져 있는 수식을 이어받아 계산 (o)
+  (2) 상태관리 함수를 false로 변경 (o)
   */
 };
 
-// 괄호를 얻어 계산대에 올리는 함수
-const getBracket = function (e) {
-  console.log(e.target.value);
-};
-
 // 계산대를 리셋해주는 함수
-acNode.addEventListener("click", () => {});
+acNode.addEventListener("click", () => {
+  resultNode.innerHTML = 0;
+  isResultDisplayed = true;
+});
+
 // 계산대에 올라간 수식을 계산해 주는 함수
-equalNode.addEventListener("click", () => {});
+equalNode.addEventListener("click", () => {
+  let result = resultNode.innerHTML;
+  if (result.trim().length !== 0) {
+    resultNode.innerHTML = math.evaluate(resultNode.innerHTML);
+  }
+  isResultDisplayed = true;
+  console.log(isResultDisplayed);
+});
 
 // 숫자 노드와 연산자 노드를 배열에 모아 놓음
-const numNodes = [zeroNode, oneNode, twoNode, threeNode, fourNode, fiveNode, sixNode, sevenNode, eightNode, nineNode];
+const numNodes = [zeroNode, oneNode, twoNode, threeNode, fourNode, fiveNode, sixNode, sevenNode, eightNode, nineNode, leftBracketNode, rightBracketNode];
 const operatorNodes = [percentNode, divideNode, timesNode, minusNode, dotNode, plusNode];
-const bracketNodes = [leftBracketNode, rightBracketNode];
 
 // 노드 배열의 모든 원소에 대해 다음 함수 실행
 const addEventListenertoNodes = (nodes, handler) => {
@@ -80,6 +96,5 @@ const addEventListenertoNodes = (nodes, handler) => {
 
 // numNodes와 operatorNodes 배열의 각 요소에 대해 이벤트리스너 등록
 // 함수를 바로 실행해 버려서는 안 되고, 이벤트가 발생하는 순간 실행되도록 함수명만 넣어줘야 한다.
-addEventListenertoNodes(numNodes, getNum);
+addEventListenertoNodes(numNodes, getValue);
 addEventListenertoNodes(operatorNodes, getOperator);
-addEventListenertoNodes(bracketNodes, getBracket);
