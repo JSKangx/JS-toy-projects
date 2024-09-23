c5778a47b8caeaf79c50dc0e1ed76f48bc74a81d
Jinsu <injnamek@gmail.com>
2024-09-23 16:38:35 +0900

    할 일 목록 화면에 출력하는 로직 작성하고, gap 으로 간격 설정

--- 03-todoList/main.js
+++ 03-todoList/main.js
@@ -1,1 +1,23 @@
 "use strict";
+
+let myForm = document.getElementById("myForm");
+let list = document.getElementById("list");
+
+myForm.addEventListener("submit", function (e) {
+  e.preventDefault(); // 새로고침 방지
+  // 유저의 입력값에 접근하기
+  let inputNode = document.getElementById("input");
+  let newTodo = inputNode.value;
+  console.log(newTodo);
+  // 폼 제출 후 input 칸 공백으로 초기화
+  inputNode.value = "";
+
+  // 폼을 제출할 때마다 li 태그 생성
+  let createList = document.createElement("li");
+  // 생성된 li 태그에 유저의 입력 추가.
+  createList.innerHTML = `<input type="checkbox" /> ${newTodo}`;
+  console.log(createList);
+
+  // 생성된 li를 ul에 출력
+  list.insertBefore(createList, list.childNodes[0]);
+});
