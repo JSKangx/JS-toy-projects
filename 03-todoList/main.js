f7e30792691ed1e1236d686e9c65e55eb50955cc
Jinsu <injnamek@gmail.com>
2024-10-01 21:30:28 +0900

    input 유효성 검사 로직 업데이트

--- 03-todoList/main.js
+++ 03-todoList/main.js
@@ -11,7 +11,7 @@ myForm.addEventListener("submit", function (e) {
   // 유저의 입력값을 '텍스트 노드'로 만들어 변수에 할당.
   // 이게 텍스트 노드여야지만 appendChild의 인수가 될 수 있다.
   let newTodo = document.createTextNode(inputNode.value);
-  console.log(newTodo.data);
+  // console.log(newTodo.data);
   // input을 만들고 checkbox 속성을 지정
   let checkboxNode = document.createElement("input");
   checkboxNode.setAttribute("type", "checkbox");
@@ -26,14 +26,16 @@ myForm.addEventListener("submit", function (e) {
   // 폼을 제출할 때마다 li 태그 생성
   let createList = document.createElement("li");
   createList.setAttribute("class", "todo");
-  // 생성된 li 태그에
-  if (newTodo.data.trim().length !== 0) {
+  // input에 입력된 것이 있을 때만
+  if (inputNode.value) {
     // (1) 유저의 입력 추가.
     createList.appendChild(newTodo);
     // (2) 체크박스 인풋 추가.
     createList.insertBefore(checkboxNode, createList.childNodes[0]);
     // (3) 제거 버튼 추가
     createList.appendChild(deleteButton);
+    // 생성된 li를 ul에 출력
+    list.insertBefore(createList, list.childNodes[0]);
     // 이게 실행되지 않는데도, 자꾸 크기가 늘어남..
   }
 
@@ -47,9 +49,6 @@ myForm.addEventListener("submit", function (e) {
       this.parentNode.style.textDecoration = "unset";
     }
   });
-
-  // 생성된 li를 ul에 출력
-  list.insertBefore(createList, list.childNodes[0]);
 
   // 폼 제출 후 input 칸 공백으로 초기화
   inputNode.value = "";
